d311bbbd0c67e4715355f4e2febeb150f02935b3
Steven Waskey <accounts@stevenawaskey.com>
2023-11-12 14:05:44 -0500

    config updaets.

--- up.sh
+++ up.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker-compose -f docker-compose.prod.yml up -d workspace nginx mysql php-fpm redis certbot
+docker-compose -f "docker-compose.$1.yml" up -d workspace nginx mysql php-fpm redis certbot
